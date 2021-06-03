c9fb6932f7b33dac8c950af4be0e4d2e4fd3b96f
nawan44 <rachmat.d.gunawan@gmail.com>
2021-06-03 23:58:55 +0700

    footer

---
+++ src/components/footer.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="app-footer">
+        <div class="address-detail">
+            <div class="contact-details">
+                <div class="title-contact">
+                    <i class='fas fa-phone-alt'></i>
+                    <h4>
+                        Call</h4>
+                </div>
+                <p>
+                    <a href="https://wa.me/+6281xxxxxxxx" class="footer-text">081xxxxxxxx</a>
+                </p>
+            </div>
+            <div class="contact-details">
+                <div class="title-contact">
+                    <i class='fas fa-map-marker-alt'></i>
+                    <h4>
+                        Visit</h4>
+                </div>
+                <p>
+                    <a href="https://goo.gl/maps/GpjMTqSTKenzxpkw8" class="footer-text">Jl. KH AHmad Junaedi 2 No 15
+                        <br/>Bekasi</a>
+                </p>
+            </div>
+            <div class="contact-details">
+                <div class="title-contact">
+                    <i class='fas fa-envelope'></i>
+                    <h4>
+                        Email</h4>
+                </div>
+                <p>
+                    <a href="mailto:info@rsmartcomm.com" class="footer-text">info@rachwn.com</a>
+                </p>
+            </div>
+        </div>
+        <div class="social">
+
+            <div class="social-medsos-center">
+                <div class="container-medsos">
+                    <a href="https://facebook.com">
+                        <i class='fab fa-facebook-square font-icon'></i>
+                    </a>
+                    <a href="https://instagram.com">
+                        <i class='fab fa-instagram font-icon' ></i>
+                    </a>
+
+                    <a href="https://linkedin.com">
+                        <i class='fab fa-linkedin font-icon'></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-copy">
+            <p class="copyright">&copy; Rachwan all right reserved.</p>
+        </div>
+    </div>
+</template>
+
+<style scoped="scoped">
+    @import "./style/footer.css";
+    
+</style>
