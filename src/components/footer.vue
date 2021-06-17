4c800cf28d292cc56feb696b25b92aa661b85b09
nawan44 <rachmat.d.gunawan@gmail.com>
2021-06-18 00:17:11 +0700

    fontawesome 5

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,72 +1,79 @@
 <template>
-    <div class="app-footer">
-    
-        <div class="address-detail">
-            <div class="contact-details">
-                <div class="title-contact">
-                    <i class='fas fa-phone-alt'></i>
-                    <h4>
-                        Call</h4>
-                </div>
-                <p>
-                    <a href="https://wa.me/+62813xxx21336453" class="footer-text">0813xxx</a>
-                </p>
-            </div>
-            <div class="contact-details">
-                <div class="title-contact">
-                    <i class='fas fa-map-marker-alt'></i>
-                    <h4>
-                        Visit</h4>
-                </div>
-                <p>
-                    <a href="https://g.page/cuci-dan-servis-sofa?share" class="footer-text">
-Griya bandung asri 2 blok F 2 no 6, Cipagalo, Kec. Bojongsoang
-
-                        <br/>Bandung</a>
-                </p>
-            </div>
-            <div class="contact-details">
-                <div class="title-contact">
-                    <i class='fas fa-envelope'></i>
-                    <h4>
-                        Email</h4>
-                </div>
-                <p>
-                    <a href="mailto:info@rsmartcomm.com" class="footer-text">info@rachwn.com</a>
-                </p>
-            </div>
+  <div class="app-footer">
+    <div class="address-detail">
+      <div class="contact-details">
+        <div class="title-contact">
+          <i class="fas fa-phone-alt"></i>
+          <h4>
+            Call
+          </h4>
         </div>
-        <div class="social">
-
-            <div class="social-medsos-center">
-                <div class="container-medsos">
-                    <a href="https://facebook.com">
-<i class="fa fa-facebook-square" style="font-size:24px"></i>                    </a>
-                    <a href="https://instagram.com">
-                        <i class='fab fa-instagram font-icon' ></i>
-                    </a>
-
-                    <a href="https://linkedin.com">
-                        <i class='fab fa-linkedin font-icon'></i>
-                    </a>
-                </div>
-            </div>
+        <p>
+          <a href="https://wa.me/+62813xxx21336453" class="footer-text"
+            >0813xxx</a
+          >
+        </p>
+      </div>
+      <div class="contact-details">
+        <div class="title-contact">
+          <i class="fas fa-map-marker-alt"></i>
+          <h4>
+            Visit
+          </h4>
         </div>
-        <div class="col-copy">
-            <p class="copyright">&copy; Rachwan all right reserved.</p>
+        <p>
+          <a
+            href="https://g.page/cuci-dan-servis-sofa?share"
+            class="footer-text"
+          >
+            Griya bandung asri 2 blok F 2 no 6, Cipagalo, Kec. Bojongsoang
+
+            <br />Bandung</a
+          >
+        </p>
+      </div>
+      <div class="contact-details">
+        <div class="title-contact">
+          <i class="fas fa-envelope"></i>
+          <h4>
+            Email
+          </h4>
         </div>
+        <p>
+          <a href="mailto:info@rsmartcomm.com" class="footer-text"
+            >info@rachwn.com</a
+          >
+        </p>
+      </div>
     </div>
+    <div class="social">
+      <div class="social-medsos-center">
+        <div class="container-medsos">
+          <a href="https://facebook.com">
+           <i class='fab fa-facebook-square  font-icon '></i>
+          </a>
+          <a href="https://instagram.com">
+            <i class="fab fa-instagram font-icon"></i>
+          </a>
+
+          <a href="https://linkedin.com">
+            <i class="fab fa-linkedin font-icon"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="col-copy">
+      <p class="copyright">&copy; Rachwan all right reserved.</p>
+    </div>
+  </div>
 </template>
 <script>
 export default {
   name: "AppFooter",
-  components: {
-    
-  }
+  components: {}
 };
 </script>
 
 <style scoped="scoped">
-     @import "../assets/style/footer.css";
-    
+@import "../assets/style/footer.css";
 </style>
