4a439281b00016bb1b9423df1ddb0d2dcdb18f1c
nawan44 <rachmat.d.gunawan@gmail.com>
2021-06-21 08:48:02 +0700

    navbar

--- src/components/Navmenu.vue
+++ src/components/Navmenu.vue
@@ -1,30 +1,31 @@
 <template>
-    <div class="nav-vue">
-         <b-navbar toggleable="lg" type="dark" variant="dark">
-        <img src="../assets/logo/logo-nav.png" class="logo-navbar">
-
-
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-    <b-collapse id="nav-collapse" is-nav>
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-     
- <b-navbar-nav>
-        <b-nav-item href="#about" class="nav-title">About</b-nav-item>
-        <b-nav-item href="#services" class="nav-title">Services</b-nav-item>
-        <b-nav-item href="#work" class="nav-title">Work</b-nav-item>
-        <b-nav-item href="#contact" class="nav-title">Contact</b-nav-item>
-        
-      </b-navbar-nav>
-
-       
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-    </div>
+  <div class="nav-vue">
+    <b-navbar toggleable="sm" class=" fixed-top">
+      <img src="../assets/logo/logo-nav.png" class="logo-navbar" />
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="nav-link">
+          <b-nav-item href="#about" class="nav-title">About</b-nav-item>
+          <b-nav-item href="#services" class="nav-title">Services</b-nav-item>
+          <b-nav-item href="#work" class="nav-title">Work</b-nav-item>
+          <b-nav-item href="#contact" class="nav-title">Contact</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 <style scoped>
 @import "../assets/style/navmenu.css";
+.navbar.navbar-toggler.icon-bar {
+  background: white !important;
+}
+
+@media only screen and (max-width: 990px) {
+  .navbar {
+    padding: 0.5rem !important;
+  }
+  .nav-link {
+    margin: 0 10px 0 10px;
+  }
+}
 </style>
